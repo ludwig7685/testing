f826ace36dcf452a049f2df70de23e61d4ba0ae7
expressalts <142906136+expressalts@users.noreply.github.com>
2023-08-23 21:47:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 const modeToggle = document.getElementById("mode-toggle");
-const expressalts = document.querySelector(".expressalts");
-const greenExpressalts = document.querySelector(".green-expressalts");
 
 modeToggle.addEventListener("change", () => {
   if (modeToggle.checked) {
     document.body.style.backgroundColor = "white";
-    expressalts.style.color = "black";
-    greenExpressalts.style.color = "black";
   } else {
     document.body.style.backgroundColor = "black";
-    expressalts.style.color = "white";
-    greenExpressalts.style.color = "green";
   }
 });
